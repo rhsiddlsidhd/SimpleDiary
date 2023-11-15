fe3fc5625bd5bb16fc7db243275ae4b2107f71b4
young <rhsiddlsidhd@nate.com>
2023-11-15 22:51:30 +0900

    Feat:API호출

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import LifeCycle from "./LifeCycle";
+
+//jsonplaceholder 에서 무료 API사용
+//https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
 
-  // console.log(data);
-
   const dataId = useRef(0);
+
+  //API
+  const getDate = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/comments`
+    ).then((res) => res.json());
+    console.log(res);
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  //API호출확인
+  useEffect(() => {
+    getDate();
+  }, []);
 
   const onCraete = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -42,7 +66,6 @@ function App() {
 
   return (
     <div className="App">
-      <LifeCycle />
       <DiaryEditor onCraete={onCraete} />
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
